c2dece56acb16e07dd8fcc58eee8bc3a76472026
Aditya Shinde <adityashinde91023@gmail.com>
2026-01-12 16:14:57 +0530

    index.js fixed

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(
     allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
-app.options("*", cors());
+app.use(cors());
 
 /* ======================================================
    🧠 BODY PARSING
